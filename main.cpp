91d207d1e4bf56f66d1cce00703560073eb0c86e
RONALD EDUARDO EDUARDO <>
2021-04-18 22:22:47 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std; 
+
+int main() {
+  int edad=0;
+  int meses=0;
+
+  cout<<"Digite su edad\n";
+  cin>> edad;
+  meses= edad*12;
+
+  cout<< " Desde su nacimiento han transcurrido "<<meses<<" meses";
+  }
